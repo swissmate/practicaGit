eeaf4f36e542c05bf88c76bf07162cbe36add770
swissmate <swissomatic7@gmail.com>
2014-03-14 11:37:06 +0100

    added mods

--- GitAux.java
+++ GitAux.java
@@ -12,6 +12,7 @@ public class GitAux {
     
     public void metodoAlumno1(){
         System.out.println("metodo 1");
+        System.out.println("modificado por alumno 1"); // alumno 1
     }
     
     public void metodoAlumno2(){
@@ -20,5 +21,7 @@ public class GitAux {
     
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
+        // modificaciones alumno 1
+        System.out.println("Alumno 1 inserta esta sentencia");
     }
 }
